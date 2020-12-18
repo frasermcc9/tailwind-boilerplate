a451e4dfe8834e86444e206425ab0b5058ba219e
Fraser McCallum <fraser@themccallums.net.nz>
2020-12-18 23:03:33 +1300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./styles/tailwind.output.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="text-red-500">
+                <p>Welcome to Tailwind!</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
